d70951f292072649460a7202e5f154abee950609
Sudarshan Raghunathan <rdarshan@gmail.com>
2012-06-19 22:43:42 -0400

    Add distutils setup package

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name="PyUFGet",
+      version="0.9",
+      description="A Python interface to the University of Florida Sparse Matrix Collection",
+      author="Sudarshan Raghunathan",
+      author_email="rdarshan@gmail.com",
+      url="http://www.github.com/drdarshan/PyUFGet",
+      packages=["PyUFGet"])
