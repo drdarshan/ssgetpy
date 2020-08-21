c25d51b0c40ed111e5c32290e70b208684246f76
Sudarshan Raghunathan <rdarshan@gmail.com>
2020-08-20 19:50:07 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="ssget",
-      version="0.92",
+      version="1.0",
       description="A Python interface to the SparseSuite Matrix Collection",
       author="Sudarshan Raghunathan",
       author_email="rdarshan@gmail.com",
@@ -12,5 +12,6 @@ setup(name="ssget",
             "ssget = ssget.query:cli",
         ],
       },
+      python_requires='>3.5.2',
       install_requires=['requests>2.22', 'tqdm>4.48']
 )
