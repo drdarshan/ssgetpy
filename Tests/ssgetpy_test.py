497410d3ddf6a49747e79575baff8e8a231c2b4b
Sudarshan Raghunathan <darshan@alum.mit.edu>
2020-08-23 11:18:28 -0700

    Format files using black

--- Tests/ssgetpy_test.py
+++ Tests/ssgetpy_test.py
@@ -13,15 +13,15 @@ class TestPyUFGet(unittest.TestCase):
         matrices = p.search("HB/*")
         for matrix in matrices:
             self.assertEqual(matrix.group, "HB")
-    
+
     def test_search_by_name(self):
         matrices = p.search("c-")
         self.assertTrue(len(matrices) > 0)
         for matrix in matrices:
             self.assertTrue(matrix.name.startswith("c-"))
-    
+
     def test_filter_by_rows(self):
-        matrices = p.search(rowbounds = (None, 1000))
+        matrices = p.search(rowbounds=(None, 1000))
         self.assertTrue(len(matrices) > 0)
         for matrix in matrices:
             self.assertTrue(matrix.rows <= 1000)
@@ -31,22 +31,28 @@ class TestPyUFGet(unittest.TestCase):
         rmax = 1000
         cmin = 200
         cmax = 5000
-        matrices = p.search(rowbounds = (rmin, rmax), colbounds=(cmin, cmax))
+        matrices = p.search(rowbounds=(rmin, rmax), colbounds=(cmin, cmax))
         self.assertTrue(len(matrices) > 0)
         for matrix in matrices:
-            self.assertTrue(matrix.rows >= rmin and matrix.rows <= rmax and matrix.cols >= cmin and matrix.cols <= cmax)
+            self.assertTrue(
+                matrix.rows >= rmin
+                and matrix.rows <= rmax
+                and matrix.cols >= cmin
+                and matrix.cols <= cmax
+            )
 
     def test_filter_by_spd_true(self):
-        matrices = p.search(isspd = True)
+        matrices = p.search(isspd=True)
         self.assertTrue(len(matrices) > 0)
         for matrix in matrices:
             self.assertTrue(matrix.isspd)
 
     def test_filter_by_spd_false(self):
-        matrices = p.search(isspd = False)
+        matrices = p.search(isspd=False)
         self.assertTrue(len(matrices) > 0)
         for matrix in matrices:
             self.assertFalse(matrix.isspd)
+
 
 if __name__ == "__main__":
     unittest.main()
--- setup.py
+++ setup.py
@@ -6,20 +6,17 @@ HERE = pathlib.Path(__file__).parent
 
 README = (HERE / "README.md").read_text()
 
-setup(name="ssgetpy",
-      version="1.0",
-      description="A Python interface to the SparseSuite Matrix Collection",
-      author="Sudarshan Raghunathan",
-      author_email="darshan@alum.mit.edu",
-      url="http://www.github.com/drdarshan/ssgetpy",
-      long_description=README,
-      long_description_content_type="text/markdown",
-      packages=["ssgetpy"],
-      entry_points={
-        "console_scripts": [
-            "ssgetpy = ssgetpy.query:cli",
-        ],
-      },
-      python_requires='>3.5.2',
-      install_requires=['requests>=2.22', 'tqdm>=4.48']
+setup(
+    name="ssgetpy",
+    version="1.0",
+    description="A Python interface to the SparseSuite Matrix Collection",
+    author="Sudarshan Raghunathan",
+    author_email="darshan@alum.mit.edu",
+    url="http://www.github.com/drdarshan/ssgetpy",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    packages=["ssgetpy"],
+    entry_points={"console_scripts": ["ssgetpy = ssgetpy.query:cli",],},
+    python_requires=">3.5.2",
+    install_requires=["requests>=2.22", "tqdm>=4.48"],
 )
